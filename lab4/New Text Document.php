75752333286191c8af48b9f711d8c993752324b4
Abu Huryra <124706375+huryra08@users.noreply.github.com>
2023-03-11 01:35:06 +0600

    Add files via upload

---
+++ lab4/New Text Document.php
@@ -0,0 +1,31 @@
+<?php
+//Time zone of Bangladesh
+date_default_timezone_set("Asia/Dacca");
+
+//Checks cookie settings
+if (!isset($_COOKIE['lastVisited'])) {
+    $lastVisited = "This is my first web lab";
+    echo $lastVisited;
+    setcookie('lastVisited', date('d-m-Y', time()));
+} else if (isset($_COOKIE['lastVisited'])) {
+    $lastVisited = $_COOKIE['lastVisited'];
+    setcookie('lastVisited', date('d-m-Y', time()));
+    echo "You've last visited this site on: " . $lastVisited;
+    echo "<br> And the time was : " . date('H:i:s') . " (UTC+6)";
+} else
+    echo "Something went wrong!";
+?>
+
+
+
+<!-- 
+    //old code
+date_default_timezone_set("Asia/Dacca");
+if (isset($_COOKIE['lastVisited'])) {
+    $lastVisited = $_COOKIE['lastVisited'];
+} else {
+    $lastVisited = "Your're Visiting this site for the first time";
+}
+setcookie('lastVisited', date('d-m-Y', time()));
+echo "Visit: " . $lastVisited;
+echo "<br> Current Time: " . date('H:i:s'); -->
